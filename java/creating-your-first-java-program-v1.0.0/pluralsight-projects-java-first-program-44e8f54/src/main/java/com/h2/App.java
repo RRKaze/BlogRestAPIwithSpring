5208eaab8f7ae04cd442a0a442dd4c79adcececd
Xinyi Wang <xinyiwang00@gmail.com>
2022-09-22 00:23:37 -0400

    initial commit

---
+++ java/creating-your-first-java-program-v1.0.0/pluralsight-projects-java-first-program-44e8f54/src/main/java/com/h2/App.java
@@ -0,0 +1,27 @@
+package com.h2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+
+    public static int doubleTheNumber(int number) {
+        return 2 * number;
+//        return -1;
+    }
+    private static int add(int[] numbers) {
+        int sum = 0;
+        for (int number :
+                numbers) {
+            sum = sum + number;
+        }
+        return sum;
+//        return -1;
+    }
+}
